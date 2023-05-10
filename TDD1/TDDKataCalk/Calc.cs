e9e7a8474bc4f3e911883e0c9864abac0e45d418
AArevyan <AArevyan@AAREVYAN-NB>
2023-05-10 10:12:48 +0200

    first commit and unit Test implementation

---
+++ TDD1/TDDKataCalk/Calc.cs
@@ -0,0 +1,19 @@
+﻿namespace TDDKataCalk;
+
+public class Calc
+{
+
+    /// <summary>
+    ///Create a simple String calculator with a method int Add(string numbers)The method can take 0, 1 or 2 numbers, and will return their sum(for an empty string it will return 0) for example “” or “1” or “1, 2”
+    ///Start with the simplest test case of an empty string and move to 1 and 2 numbers
+    ///Remember to solve things as simply as possible so that you force yourself to write tests you did not think about
+    ///Remember to refactor after each passing test
+    /// </summary>
+    /// <param name="digits"></param>
+    /// <returns></returns>
+
+    public int Add(string digits)
+    {
+        throw new NotImplementedException();
+    }
+}
